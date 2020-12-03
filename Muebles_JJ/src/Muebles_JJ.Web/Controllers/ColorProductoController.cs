3c1a2eee177490379a9c0a09d169ea0bd610f249
JuanOlaya <juanchofutbol_10@hotmail.com>
2020-12-02 21:01:49 -0500

    CRUD

--- Muebles_JJ/src/Muebles_JJ.Web/Controllers/ColorProductoController.cs
+++ Muebles_JJ/src/Muebles_JJ.Web/Controllers/ColorProductoController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Muebles_JJ.Infrastructure.Data;
+using Muebles_JJ.Infrastructure;
+
 
 namespace Muebles_JJ.Web.Controllers
 {
@@ -17,10 +19,85 @@ namespace Muebles_JJ.Web.Controllers
             _context = context;
         }
 
+        //GET: ColorProducto
         public async Task<IActionResult> Index()
         {
             var listado = await _context.ColorProducto.ToListAsync();
             return View(listado);
+        }
+
+        //GET: ColorProducto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nombre")] ColorProducto colorProducto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(colorProducto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colorProducto);
+        }
+
+        [HttpGet]
+        // GET: ColorProducto/Edit
+        public async Task<IActionResult> Edit(int? IdColor)
+        {
+            if (IdColor == null)
+            {
+                return NotFound();
+            }
+
+            ColorProducto model = await _context.ColorProducto.FindAsync(IdColor);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int IdColor, [Bind("IdColor,Nombre")] ColorProducto colorProducto)
+        {
+            if (IdColor != colorProducto.IdColor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(colorProducto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ColorProductoExists(colorProducto.IdColor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colorProducto);
+        }
+
+
+        private bool ColorProductoExists(int IdColor)
+        {
+            return _context.ColorProducto.Any(c => c.IdColor == IdColor);
         }
     }
 }
