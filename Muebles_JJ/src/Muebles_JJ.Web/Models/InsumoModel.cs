14f8690861a6fe05663640a96040cd3001c1c637
JuanOlaya <juanchofutbol_10@hotmail.com>
2020-12-13 22:50:39 -0500

    Acutalizacion

---
+++ Muebles_JJ/src/Muebles_JJ.Web/Models/InsumoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Muebles_JJ.Infrastructure;
+
+namespace Muebles_JJ.Web.Models
+{
+    public class InsumoModel
+    {
+        public InsumoModel()
+        {
+            oInsumo = new Insumo();
+            oUnidadMedida = new Unidadmedida();
+        }
+        public Insumo oInsumo { get; set; }
+
+        public Unidadmedida oUnidadMedida { get; set; }
+    }
+}
